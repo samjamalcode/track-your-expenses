3ffe9f88715dde7f65515397fe5945b7aaa139ef
samjamalcode <>
2023-12-10 00:18:51 +0000

    Add 'budget' to the class Expense

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
+
+# Define the Expense class: Created the Expense class to represent user expenses.
 class Expense:
-    def __init__(self, name, category, amount) -> None:
-        # Initializes an Expense object with a name, category, and amount.
+    def __init__(self, name, category, amount, budget) -> None:
         self.name = name
         self.category = category
         self.amount = amount
+        self.budget = budget  
 
     def __repr__(self):
-        # Returns a string representation of the Expense object.
-        return f"<Expense: {self.name}, {self.category}, ${self.amount:.2f} >"
+        return f"<Expense: {self.name}, {self.category}, ${self.amount:.2f}, Budget: ${self.budget:.2f} >"
