f26cc5ae15429152c0af34c5bc9fdf33270bebbb
samjamalcode <>
2023-12-10 00:34:58 +0000

    Define the main function

--- run.py
+++ run.py
@@ -9,7 +9,19 @@ class Expense:
         self.name = name
         self.category = category
         self.amount = amount
-        self.budget = budget  
+        self.budget = budget
 
     def __repr__(self):
         return f"<Expense: {self.name}, {self.category}, ${self.amount:.2f}, Budget: ${self.budget:.2f} >"
+
+
+# Set up the main function to organize the expense tracking process
+def main():
+    print(f"🎯 Running Expense Tracker!")
+    expense_file_path = "expenses.csv"
+
+
+# Get user input for budget
+# Get user input for expense
+# Write the expense to a file
+# Read file and summarize expenses
